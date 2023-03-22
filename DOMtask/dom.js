ccf61e8cc6f584e3effa5541eee303ad887b3b2f
kumarshailesh9 <shaileshkumar9@gmail.com>
2023-03-22 15:01:48 +0530

    DOM first task

---
+++ DOMtask/dom.js
@@ -0,0 +1,5 @@
+var header = document.getElementById('main-header');
+header.style.border ='5px solid black';
+var additem =document.getElementsByClassName('title');
+additem[0].style.fontWeight = 'bold';
+additem[0].style.color='green';
