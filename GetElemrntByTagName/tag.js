ab30c3e91af6e30fbe591bb0d5706479841d63d8
kumarshailesh9 <shaileshkumar9@gmail.com>
2023-03-23 13:12:29 +0530

    get element by tag name

---
+++ GetElemrntByTagName/tag.js
@@ -0,0 +1,10 @@
+var li = document.getElementsByTagName('li');
+//li[0].style.backgroundColor='red';
+
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor="yellow";
+}
+
+for(var i=0;i<li.length;i++){
+    li[i].style.fontWeight="bold";
+}
