562644a9fba6bc6e029f3dab01515b56d19c592d
kumarshailesh9 <shaileshkumar9@gmail.com>
2023-03-23 15:11:47 +0530

    QuerySelector && QuerySelectorALL

---
+++ QuerySelector/qsa.js
@@ -0,0 +1,10 @@
+// QuerySelectorAll
+
+var secondItem = document.querySelectorAll('li');
+secondItem[1].style.color = 'green';
+
+var odd = document.querySelectorAll('li:nth-child(odd)');
+
+for(var i=0;i<odd.length;i++){
+    odd[i].style.backgroundColor ='green';
+}
---
+++ QuerySelector/query.js
@@ -0,0 +1,8 @@
+// QuerySelector
+
+var secondItemm = document.querySelector('.list-group-item:nth-child(2)');
+secondItemm.style.backgroundColor='green';
+
+var invisible = document.querySelector('.list-group-item:nth-child(3)');
+invisible.style.visibility='hidden';
+
