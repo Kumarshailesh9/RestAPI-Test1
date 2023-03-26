9ed4fc7a33600bd15d1dff4ceb08434baaa43e1c
kumarshailesh9 <shaileshkumar9@gmail.com>
2023-03-26 23:49:50 +0530

    store Object on localStorage

---
+++ StoreUserInRightFormat/main.js
@@ -0,0 +1,25 @@
+
+function bookAppoiment(event){
+    event.preventDefault();
+    const name = event.target.name.value;
+    //localStorage.setItem('user',name);
+    const email = event.target.mail.value;
+    //localStorage.setItem('emailID',email);
+    const mo = event.target.mob.value;
+    //localStorage.setItem('number',mo);
+    const dt = event.target.date.value;
+    //localStorage.setItem('date',dt);
+    const t = event.target.time.value;
+    //localStorage.setItem('time',t);
+
+    let userInfo = {
+        name,
+        email,
+        mo,
+        dt,
+        t
+    };
+
+    localStorage.setItem('userDatails',JSON.stringify(userInfo));
+
+}
